a31c5a1813a2c6524c03a886641db902bac4a9f3
Barbara Cabrera <barbara.cabrera@liferay.com>
2023-12-12 12:41:24 +0100

    LPS-202230 Add necessary methods to the configuration Display Context

--- modules/apps/message-boards/message-boards-web/src/main/java/com/liferay/message/boards/web/internal/display/context/MBConfigurationDisplayContext.java
+++ modules/apps/message-boards/message-boards-web/src/main/java/com/liferay/message/boards/web/internal/display/context/MBConfigurationDisplayContext.java
@@ -5,9 +5,25 @@
 
 package com.liferay.message.boards.web.internal.display.context;
 
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.VerticalNavItem;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.VerticalNavItemList;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.VerticalNavItemListBuilder;
+import com.liferay.message.boards.constants.MBPortletKeys;
+import com.liferay.petra.function.UnsafeConsumer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
+import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactoryUtil;
+import com.liferay.portal.kernel.portlet.constants.PortletPreferencesFactoryConstants;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 
+import java.util.Objects;
+
+import javax.portlet.PortletURL;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
@@ -30,7 +46,128 @@ public class MBConfigurationDisplayContext {
 			WebKeys.THEME_DISPLAY);
 	}
 
+	public String getBackURL() {
+		RequestBackedPortletURLFactory requestBackedPortletURLFactory =
+			RequestBackedPortletURLFactoryUtil.create(_httpServletRequest);
+
+		return PortletURLBuilder.create(
+			requestBackedPortletURLFactory.createControlPanelRenderURL(
+				MBPortletKeys.MESSAGE_BOARDS_ADMIN,
+				_themeDisplay.getScopeGroup(), 0, 0)
+		).buildString();
+	}
+
+	public String getNavigation() {
+		if (Validator.isNotNull(_navigation)) {
+			return _navigation;
+		}
+
+		_navigation = ParamUtil.getString(
+			_renderRequest, "navigation", "general");
+
+		return _navigation;
+	}
+
+	public VerticalNavItemList getNotificationsVerticalNavItemList() {
+		return VerticalNavItemListBuilder.add(
+			_getVerticalNavItemUnsafeConsumer("email-from")
+		).add(
+			_getVerticalNavItemUnsafeConsumer("message-added-email")
+		).add(
+			_getVerticalNavItemUnsafeConsumer("message-updated-email")
+		).build();
+	}
+
+	public PortletURL getPortletURL() {
+		if (_portletURL != null) {
+			return _portletURL;
+		}
+
+		_portletURL = PortletURLBuilder.createRenderURL(
+			_renderResponse
+		).setActionName(
+			"editConfiguration"
+		).setMVCPath(
+			"/edit_configuration.jsp"
+		).setPortletResource(
+			ParamUtil.getString(_httpServletRequest, "portletResource")
+		).setParameter(
+			"portletConfiguration", Boolean.TRUE
+		).setParameter(
+			"settingsScope",
+			PortletPreferencesFactoryConstants.SETTINGS_SCOPE_PORTLET_INSTANCE
+		).buildPortletURL();
+
+		return _portletURL;
+	}
+
+	public PortletURL getRedirect() {
+		return PortletURLBuilder.create(
+			getPortletURL()
+		).setNavigation(
+			getNavigation()
+		).buildPortletURL();
+	}
+
+	public VerticalNavItemList getSettingsVerticalNavItemList() {
+		return VerticalNavItemListBuilder.add(
+			_getVerticalNavItemUnsafeConsumer("general")
+		).add(
+			_getVerticalNavItemUnsafeConsumer("thread-priorities")
+		).add(
+			_getVerticalNavItemUnsafeConsumer("user-ranks")
+		).add(
+			PortalUtil::isRSSFeedsEnabled,
+			_getVerticalNavItemUnsafeConsumer("rss")
+		).build();
+	}
+
+	public String getSubtitle() {
+		if (Objects.equals(getNavigation(), "general")) {
+			return LanguageUtil.get(_httpServletRequest, "general-settings");
+		}
+
+		if (Objects.equals(getNavigation(), "rss")) {
+			return LanguageUtil.get(_httpServletRequest, "rss-subscription");
+		}
+
+		if (Objects.equals(getNavigation(), "thread-priorities")) {
+			return LanguageUtil.get(_httpServletRequest, "thread-settings");
+		}
+
+		if (Objects.equals(getNavigation(), "user-ranks")) {
+			return LanguageUtil.get(_httpServletRequest, "user-settings");
+		}
+
+		return LanguageUtil.get(_httpServletRequest, "email");
+	}
+
+	public String getTitle() {
+		return LanguageUtil.get(_httpServletRequest, getNavigation());
+	}
+
+	private UnsafeConsumer<VerticalNavItem, Exception>
+		_getVerticalNavItemUnsafeConsumer(String key) {
+
+		return verticalNavItem -> {
+			verticalNavItem.setActive(Objects.equals(getNavigation(), key));
+			verticalNavItem.setHref(
+				PortletURLBuilder.create(
+					getPortletURL()
+				).setNavigation(
+					key
+				).buildString());
+
+			String name = LanguageUtil.get(_httpServletRequest, key);
+
+			verticalNavItem.setId(name);
+			verticalNavItem.setLabel(name);
+		};
+	}
+
 	private final HttpServletRequest _httpServletRequest;
+	private String _navigation;
+	private PortletURL _portletURL;
 	private final RenderRequest _renderRequest;
 	private final RenderResponse _renderResponse;
 	private final ThemeDisplay _themeDisplay;
