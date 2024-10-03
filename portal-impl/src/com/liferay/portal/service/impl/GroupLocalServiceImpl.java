989e1870852eaee7fc26753ba3e381e153b43412
Joao Victor <joao.victor@liferay.com>
2024-10-03 17:27:26 -0300

    LPD-37997 Make company update sites with inherited locales, also when adding a new locale to the instance localization, and catch all available locales from instance localization for the case when the group that is being updated has inherited locales

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -3998,7 +3998,7 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 				PropsKeys.LOCALES,
 				StringUtil.merge(
 					LocaleUtil.toLanguageIds(
-						LanguageUtil.getAvailableLocales(groupId))));
+						LanguageUtil.getAvailableLocales())));
 		}
 
 		String newLanguageIds = typeSettingsUnicodeProperties.getProperty(
