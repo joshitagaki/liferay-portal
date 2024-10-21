280260ff3c1392106ea55681bd9da5923919927b
Carlos Correa <carlos.correa@liferay.com>
2024-10-21 13:57:13 +0200

    LPD-37767 Use the method that checks the exact object entry to avoid possible errors when asserting the data

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
@@ -4977,7 +4977,6 @@ public class ObjectEntryResourceTest {
 	public void testGetObjectEntriesFilteredBySystemDate() throws Exception {
 		_objectEntry1 = ObjectEntryTestUtil.addObjectEntry(
 			_objectDefinition1, _OBJECT_FIELD_NAME_1, _OBJECT_FIELD_VALUE_1);
-
 		_objectEntry2 = ObjectEntryTestUtil.addObjectEntry(
 			_objectDefinition1, _OBJECT_FIELD_NAME_1, _OBJECT_FIELD_VALUE_2);
 
@@ -5008,7 +5007,6 @@ public class ObjectEntryResourceTest {
 				_OBJECT_FIELD_NAME_1, _OBJECT_FIELD_VALUE_1,
 				URLCodec.encodeURL(fieldName + " eq 2023-09-20T10:00:00.999Z"),
 				_objectDefinition1);
-
 			_assertFilterString(
 				_OBJECT_FIELD_NAME_1, _OBJECT_FIELD_VALUE_2,
 				URLCodec.encodeURL(fieldName + " eq 2023-09-20T10:05:00Z"),
@@ -5024,8 +5022,10 @@ public class ObjectEntryResourceTest {
 				2, fieldName + " ge 2023-09-20T10:00:00Z");
 			_assertFilteredObjectEntries(
 				2, fieldName + " ge 2023-09-20T10:00:00.999Z");
-			_assertFilteredObjectEntries(
-				1, fieldName + " ge 2023-09-20T10:00:01Z");
+			_assertFilterString(
+				_OBJECT_FIELD_NAME_1, _OBJECT_FIELD_VALUE_2,
+				URLCodec.encodeURL(fieldName + " ge 2023-09-20T10:00:01Z"),
+				_objectDefinition1);
 
 			// Test case for 'gt' (greater than)
 
@@ -5054,8 +5054,10 @@ public class ObjectEntryResourceTest {
 				2, fieldName + " le 2023-09-20T10:05:00Z");
 			_assertFilteredObjectEntries(
 				2, fieldName + " le 2023-09-20T10:05:00.999Z");
-			_assertFilteredObjectEntries(
-				1, fieldName + " le 2023-09-20T10:04:00.999Z");
+			_assertFilterString(
+				_OBJECT_FIELD_NAME_1, _OBJECT_FIELD_VALUE_1,
+				URLCodec.encodeURL(fieldName + " le 2023-09-20T10:04:00.999Z"),
+				_objectDefinition1);
 
 			// Test case for 'lt' (less than)
 
