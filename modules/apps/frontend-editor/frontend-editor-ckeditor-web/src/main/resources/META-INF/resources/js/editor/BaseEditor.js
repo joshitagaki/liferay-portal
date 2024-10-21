fe4ccd84246b9d4d2321260bf1c702fb18d3f401
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2024-10-21 14:59:54 +0200

    LPD-39085 Cleanup editor global functions

--- modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/js/editor/BaseEditor.js
+++ modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/js/editor/BaseEditor.js
@@ -129,6 +129,10 @@ const BaseEditor = forwardRef(
 					return contents;
 				},
 			};
+
+			return () => {
+				delete window[name];
+			};
 		}, [contents, getHTML, name]);
 
 		return loading ? (
