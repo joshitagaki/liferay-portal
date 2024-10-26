a6ef08147b66e7a345a3680d70ea3b4455826fe5
Brian Chan <brian.chan@liferay.com>
2024-10-26 09:02:01 -0300

    LPD-37535 Rename

--- modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/asset/entry/provider/AssetListAssetEntryProviderImpl.java
+++ modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/asset/entry/provider/AssetListAssetEntryProviderImpl.java
@@ -703,7 +703,7 @@ public class AssetListAssetEntryProviderImpl
 		if (_assetListConfiguration.combineAssetsFromAllSegmentsManual()) {
 			Map<AssetListEntryAssetEntryRel, Long> groupIds = new HashMap<>();
 
-			segmentsEntryIds = _sortSegmentsByPriority(
+			segmentsEntryIds = _sort(
 				assetListEntry,
 				_getCombinedSegmentsEntryIds(assetListEntry, segmentsEntryIds));
 
@@ -1033,7 +1033,7 @@ public class AssetListAssetEntryProviderImpl
 		assetEntryQuery.setNotAnyTagIds(notAnyAssetTagIds);
 	}
 
-	private long[] _sortSegmentsByPriority(
+	private long[] _sort(
 		AssetListEntry assetListEntry, long[] segmentsEntryIds) {
 
 		return TransformUtil.transformToLongArray(
