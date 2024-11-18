d1dfcff0ac9c07ea84e432fd076dbf6f4552c70d
Brian Chan <brian.chan@liferay.com>
2024-11-18 11:49:37 +0100

    LPD-42050 Inline

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -4040,13 +4040,13 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 
 				Map<Locale, String> nameMap = group.getNameMap();
 
-				String className = group.getClassName();
-
 				if ((nameMap != null) &&
 					Validator.isNotNull(nameMap.get(defaultLocale)) &&
 					((group.getClassNameId() <= 0) ||
 					 (group.getType() == GroupConstants.TYPE_DEPOT) ||
-					 className.equals(Group.class.getName()))) {
+					 Objects.equals(
+					 	group.getClassName(),
+					 	Group.class.getName()))) {
 
 					group.setGroupKey(nameMap.get(defaultLocale));
 				}
