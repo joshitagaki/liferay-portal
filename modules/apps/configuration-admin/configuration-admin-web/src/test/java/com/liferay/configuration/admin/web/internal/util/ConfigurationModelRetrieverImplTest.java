b168a728c6839fdcbaf89900aa5dc85dad0cd4d6
Thiago Buarque <thiago.buarque@liferay.com>
2025-07-11 14:21:09 -0300

    Revert "LPD-60222 Sort (seems to contradict my previous commit but it doesn't, since this is not based on service.xml)"
    This reverts commit fb52e99e524b33595df55906ce0ba53c18c62421.

--- modules/apps/configuration-admin/configuration-admin-web/src/test/java/com/liferay/configuration/admin/web/internal/util/ConfigurationModelRetrieverImplTest.java
+++ modules/apps/configuration-admin/configuration-admin-web/src/test/java/com/liferay/configuration/admin/web/internal/util/ConfigurationModelRetrieverImplTest.java
@@ -98,12 +98,6 @@ public class ConfigurationModelRetrieverImplTest {
 
 		String pid = RandomTestUtil.randomString();
 
-		Mockito.verify(
-			configurationModelRetrieverImpl, Mockito.never()
-		).getConfiguration(
-			pid, ExtendedObjectClassDefinition.Scope.COMPANY, companyId, true
-		);
-
 		configurationModelRetrieverImpl.getConfiguration(
 			pid, ExtendedObjectClassDefinition.Scope.GROUP, groupId);
 
@@ -116,16 +110,16 @@ public class ConfigurationModelRetrieverImplTest {
 		Mockito.verify(
 			configurationModelRetrieverImpl, Mockito.never()
 		).getConfiguration(
+			pid, ExtendedObjectClassDefinition.Scope.COMPANY, companyId, true
+		);
+
+		Mockito.verify(
+			configurationModelRetrieverImpl, Mockito.never()
+		).getConfiguration(
 			pid, ExtendedObjectClassDefinition.Scope.SYSTEM, null, true
 		);
 
 		Mockito.clearInvocations(configurationModelRetrieverImpl);
-
-		Mockito.verify(
-			configurationModelRetrieverImpl, Mockito.times(1)
-		).getConfiguration(
-			pid, ExtendedObjectClassDefinition.Scope.COMPANY, companyId, false
-		);
 
 		configurationModelRetrieverImpl.getConfiguration(
 			pid, ExtendedObjectClassDefinition.Scope.GROUP, groupId, false);
@@ -134,6 +128,12 @@ public class ConfigurationModelRetrieverImplTest {
 			configurationModelRetrieverImpl, Mockito.times(1)
 		).getConfiguration(
 			pid, ExtendedObjectClassDefinition.Scope.GROUP, groupId, false
+		);
+
+		Mockito.verify(
+			configurationModelRetrieverImpl, Mockito.times(1)
+		).getConfiguration(
+			pid, ExtendedObjectClassDefinition.Scope.COMPANY, companyId, false
 		);
 
 		Mockito.verify(
