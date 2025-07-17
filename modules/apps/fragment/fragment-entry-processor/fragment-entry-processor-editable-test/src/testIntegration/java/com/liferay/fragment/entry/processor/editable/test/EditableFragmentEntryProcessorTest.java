930e7c35a7f79e82631f5dbae50eaefc879fb8d6
Brian Chan <brian.chan@liferay.com>
2025-07-17 06:52:45 +0200

    LPD-60383 Sort

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable-test/src/testIntegration/java/com/liferay/fragment/entry/processor/editable/test/EditableFragmentEntryProcessorTest.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable-test/src/testIntegration/java/com/liferay/fragment/entry/processor/editable/test/EditableFragmentEntryProcessorTest.java
@@ -1289,17 +1289,6 @@ public class EditableFragmentEntryProcessorTest {
 		Locale currentLocale = LocaleThreadLocal.getThemeDisplayLocale();
 
 		LocaleThreadLocal.setThemeDisplayLocale(
-			LocaleUtil.fromLanguageId("es_ES"));
-
-		Assert.assertThat(
-			_fragmentEntryProcessorRegistry.processFragmentEntryLinkHTML(
-				fragmentEntryLink,
-				_getFragmentEntryProcessorContext(
-					LocaleUtil.fromLanguageId("es_ES"),
-					FragmentEntryLinkConstants.EDIT)),
-			CoreMatchers.containsString("es_ES-alt"));
-
-		LocaleThreadLocal.setThemeDisplayLocale(
 			LocaleUtil.fromLanguageId("de_DE"));
 
 		Assert.assertThat(
@@ -1309,6 +1298,17 @@ public class EditableFragmentEntryProcessorTest {
 					LocaleUtil.fromLanguageId("de_DE"),
 					FragmentEntryLinkConstants.EDIT)),
 			CoreMatchers.containsString("en_US-alt"));
+
+		LocaleThreadLocal.setThemeDisplayLocale(
+			LocaleUtil.fromLanguageId("es_ES"));
+
+		Assert.assertThat(
+			_fragmentEntryProcessorRegistry.processFragmentEntryLinkHTML(
+				fragmentEntryLink,
+				_getFragmentEntryProcessorContext(
+					LocaleUtil.fromLanguageId("es_ES"),
+					FragmentEntryLinkConstants.EDIT)),
+			CoreMatchers.containsString("es_ES-alt"));
 
 		LocaleThreadLocal.setThemeDisplayLocale(currentLocale);
 	}
