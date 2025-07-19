8740bb242de45e8bd558b8d9f35cb3d671b58c5b
Jorge Díaz <jorge.diaz@liferay.com>
2025-07-20 01:31:18 +0200

    LPD-61175 Remove ignore test added in LPS-153863 because it was fixed by commit 9c7ba1a7b3e91a4a5808b848fcc015ee6aed4f0a

--- modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalInstancesTest.java
+++ modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalInstancesTest.java
@@ -41,7 +41,6 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -96,7 +95,6 @@ public class PortalInstancesTest {
 			_virtualHostsDefaultSiteName);
 	}
 
-	@Ignore
 	@Test
 	public void testGetCompanyId() {
 		_updateLayoutSetVirtualHostname(
