d9121764717bffcb7e136fd82803e11ca6b5d5d9
Brian I. Kim <il.kim@liferay.com>
2025-07-30 01:17:20 -0700

    LPD-58862 Make query selector more fluid to handle edge cases

--- modules/apps/portal-search/portal-search-web/src/main/resources/META-INF/resources/js/FacetUtil.js
+++ modules/apps/portal-search/portal-search-web/src/main/resources/META-INF/resources/js/FacetUtil.js
@@ -393,7 +393,9 @@ export default function ({namespace: portletNamespace}) {
 
 	if (portletNamespace) {
 		FacetUtil.enableInputs(
-			document.querySelectorAll(`#${portletNamespace}fm .facet-term`)
+			document.querySelectorAll(
+				`[id^="${portletNamespace}fm"] .facet-term`
+			)
 		);
 	}
 }
