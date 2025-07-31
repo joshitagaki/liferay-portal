934612e29dee385728a93784570d9a69e9d5064f
Brian Chan <brian.chan@liferay.com>
2025-07-31 09:29:40 +0800

    LPD-60844 SF

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
@@ -5778,11 +5778,13 @@ public class ObjectEntryResourceTest {
 	@Test
 	public void testGetObjectEntriesVersionsPage() throws Exception {
 		_objectDefinition1.setEnableObjectEntryVersioning(true);
-		_objectDefinition2.setEnableObjectEntryVersioning(true);
 
 		_objectDefinition1 =
 			_objectDefinitionLocalService.updateObjectDefinition(
 				_objectDefinition1);
+
+		_objectDefinition2.setEnableObjectEntryVersioning(true);
+
 		_objectDefinition2 =
 			_objectDefinitionLocalService.updateObjectDefinition(
 				_objectDefinition2);
