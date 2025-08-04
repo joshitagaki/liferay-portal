1f19abba3aed03b398f666d45df494eee4abaff8
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2025-08-04 13:11:13 +0200

    LPD-61755 Avoid chaining and sending an empty array instead of an OD (we only really need to check the task configuration data), but can't remove try-finally, cause if this test fails then the user by default will stay deactivated and it can affect other tests

--- modules/apps/batch-engine/batch-engine-test/src/testIntegration/java/com/liferay/batch/engine/internal/test/BatchEngineBundleTrackerTest.java
+++ modules/apps/batch-engine/batch-engine-test/src/testIntegration/java/com/liferay/batch/engine/internal/test/BatchEngineBundleTrackerTest.java
@@ -169,7 +169,6 @@ public class BatchEngineBundleTrackerTest {
 	public void testProcessBatchEngineBundleUsesActiveAdministratorUser()
 		throws Exception {
 
-		User adminUser = null;
 		int originalStatus = -1;
 		long originalUserId = -1;
 
@@ -181,8 +180,10 @@ public class BatchEngineBundleTrackerTest {
 				initialBatchEngineImportTaskReference::set, "batch11",
 				"/batch11/data.batch-engine-data.json");
 
-			originalUserId = initialBatchEngineImportTaskReference.get(
-			).getUserId();
+			BatchEngineImportTask batchEngineImportTask1 =
+				initialBatchEngineImportTaskReference.get();
+
+			originalUserId = batchEngineImportTask1.getUserId();
 
 			User originalUser = _userLocalService.getUser(originalUserId);
 
@@ -190,13 +191,13 @@ public class BatchEngineBundleTrackerTest {
 
 			originalStatus = originalUser.getStatus();
 
-			_userLocalService.updateStatus(
+			originalUser = _userLocalService.updateStatus(
 				originalUser, WorkflowConstants.STATUS_INACTIVE,
 				new ServiceContext());
 
 			Assert.assertFalse(originalUser.isActive());
 
-			adminUser = UserTestUtil.addCompanyAdminUser(
+			User adminUser = UserTestUtil.addCompanyAdminUser(
 				_companyLocalService.getCompany(
 					TestPropsValues.getCompanyId()));
 
@@ -210,9 +211,11 @@ public class BatchEngineBundleTrackerTest {
 				fallbackBatchEngineImportTaskReference::set, "batch11",
 				"/batch11/data.batch-engine-data.json");
 
+			BatchEngineImportTask batchEngineImportTask2 =
+				fallbackBatchEngineImportTaskReference.get();
+
 			User fallbackBatchEngineImportTaskUser = _userLocalService.getUser(
-				fallbackBatchEngineImportTaskReference.get(
-				).getUserId());
+				batchEngineImportTask2.getUserId());
 
 			Assert.assertTrue(fallbackBatchEngineImportTaskUser.isActive());
 
@@ -224,10 +227,10 @@ public class BatchEngineBundleTrackerTest {
 						userRole.getName())));
 		}
 		finally {
-			_userLocalService.updateStatus(
-				originalUserId, originalStatus, new ServiceContext());
-
-			_userLocalService.deleteUser(adminUser);
+			if (originalUserId != -1) {
+				_userLocalService.updateStatus(
+					originalUserId, originalStatus, new ServiceContext());
+			}
 		}
 	}
 
