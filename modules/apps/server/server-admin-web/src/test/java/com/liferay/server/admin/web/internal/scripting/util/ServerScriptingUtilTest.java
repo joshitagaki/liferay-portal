832a113dd41ac28f6306c7390cab13d3e542a570
Jerry Adrianoo <jerry.adriano@liferay.com>
2025-08-15 20:31:45 -0300

    LPD-62373 Add unit tests to expose issue and cover execute method usages

---
+++ modules/apps/server/server-admin-web/src/test/java/com/liferay/server/admin/web/internal/scripting/util/ServerScriptingUtilTest.java
@@ -0,0 +1,84 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.server.admin.web.internal.scripting.util;
+
+import com.liferay.petra.io.unsync.UnsyncByteArrayOutputStream;
+import com.liferay.petra.io.unsync.UnsyncPrintWriter;
+import com.liferay.portal.kernel.scripting.ScriptingException;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import java.util.Collections;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+/**
+ * @author Jerry Adriano
+ */
+public class ServerScriptingUtilTest {
+
+	@ClassRule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		new LiferayUnitTestRule();
+
+	@Test
+	public void testExecuteGroovyScript() throws ScriptingException {
+		UnsyncByteArrayOutputStream unsyncByteArrayOutputStream =
+			new UnsyncByteArrayOutputStream();
+
+		UnsyncPrintWriter unsyncPrintWriter = new UnsyncPrintWriter(
+			unsyncByteArrayOutputStream);
+
+		ServerScriptingUtil.execute(
+			HashMapBuilder.<String, Object>put(
+				"out", unsyncPrintWriter
+			).build(),
+			"groovy", "print 1 + 1");
+
+		unsyncPrintWriter.flush();
+
+		Assert.assertEquals("2", unsyncByteArrayOutputStream.toString());
+	}
+
+	@Test
+	public void testExecuteGroovyScriptWithException() {
+		try {
+			ServerScriptingUtil.execute(
+				Collections.emptyMap(), "groovy",
+				"throw new UnsupportedOperationException();");
+
+			Assert.fail();
+		}
+		catch (ScriptingException scriptingException) {
+			String message = scriptingException.getMessage();
+
+			Assert.assertTrue(
+				message, message.startsWith("Unable to execute script:"));
+			Assert.assertTrue(
+				message,
+				message.contains(
+					"Line 1: throw new UnsupportedOperationException();"));
+		}
+	}
+
+	@Test
+	public void testExecuteWithUnsupportedLanguage() {
+		try {
+			ServerScriptingUtil.execute(
+				Collections.emptyMap(), "shell", "return 1 + 1");
+
+			Assert.fail();
+		}
+		catch (ScriptingException scriptingException) {
+			String message = scriptingException.getMessage();
+
+			Assert.assertTrue(message, message.contains("shell"));
+		}
+	}
+
+}
